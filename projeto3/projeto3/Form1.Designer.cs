3eb16af4824f1f8af6bd33c594db8d5579284524
Gustavo Ramos <86236402+gustavoramos16@users.noreply.github.com>
2022-09-13 15:54:26 -0300

    Add files via upload

---
+++ projeto3/projeto3/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace projeto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 42);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Gerar Parcelas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor (R$):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(168, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "N° Parcelas:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 34);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pagar Parcela";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 17;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 79);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(306, 31);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(12, 228);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(300, 31);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(480, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(335, 379);
+            this.listBox1.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 422);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(480, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Parcelas:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(80, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 34);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Confirmar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 351);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 34);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Cancelar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 25);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Data do Pagamento:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(324, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 34);
+            this.button5.TabIndex = 27;
+            this.button5.Text = "Pagar Todas";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 462);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ projeto3/projeto3/Form1.cs
@@ -0,0 +1,199 @@
+﻿/* ******************************************************************
+ * Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 06 / 09 / 2022
+ * Autores do Projeto: Rodrigo Lopes Marques
+ *                     Gustavo Otacílio
+ *
+ * Turma: 2F
+ * Projeto do 3° Bimestre
+ * Observação: <colocar se houver>
+ * 
+ * ******************************************************************/
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * COMPONENTES E SUAS FUNÇÕES:
+ * 
+ * textbox1 - Caixa de texto para o envio do valor da compra
+ * textbox2 - Caixa de texto para o envio do número de parcelas da compra
+ * 
+ * label1 - Envio de mensagens de erro na parte de cadastrar parcelas de uma compra
+ * label2 - Envio de mensagens de erro e informações ao pagar uma parcela
+ * label3 - Envio do valor total restante da compra
+ * label4 - Etiqueta para o usuario identificar o que é textbox1
+ * label5 - Etiqueta para o usuario identificar o que é textbox2
+ * label6 - Etiqueta para o usuario identificar o que o conteúdo da listbox1
+ * label7 - Etiqueta para o usuario identificar o que o dateTimePicker2
+ * 
+ * button1 - Botão para efetuar o cadastro das parcelas de uma compra
+ * button2 - Botão para efetuar o pagamento da parcela
+ * button3 - Botão para confirmar o pagamento da parcela
+ * button4 - Botão para cancelar o pagamento da parcela
+ * button5 - Botão para efetuar o pagamento de todas as parcelas pendentes
+ * 
+ * listbox1 - Lista para guardar as parcelas de uma compra
+ * 
+ * dateTimePicker1 - Calendário para simular o dia da compra
+ * dateTimePicker2 - Calendário para simular o dia do pagamento da parcela
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto3
+{
+    public partial class Form1 : Form
+    {
+        double valorTotal;
+        int indexP;
+        double valorParcela;
+        int qtdeParcelas;
+        DateTime dataCompra;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resetar();
+            label2.Text = "";
+            // Validação
+            double numero;
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                label1.Text = "Campo valor e/ou número parcelas vazias!";
+                return;
+            }
+            else if (!Double.TryParse(textBox1.Text, out numero) || !Double.TryParse(textBox2.Text, out numero))
+            {
+                label1.Text = "Valor e/ou Número parcelas inválidas!";
+                return;
+            }
+            else if (valorTotal != 0)
+            {
+                label1.Text = "Pague todas as parcelas antes de cadastrar mais.";
+                return;
+            }
+
+            // Recebe os dados
+            valorTotal = double.Parse(textBox1.Text);
+            qtdeParcelas = int.Parse(textBox2.Text);
+            dataCompra = dateTimePicker1.Value.Date;
+
+            label3.Text = String.Format("Total a pagar = {0:C2}", valorTotal);
+            valorParcela = valorTotal / qtdeParcelas;
+
+
+            // Cria as parcelas
+            DateTime dataParcela = dataCompra;
+            for (int i = 0; i < qtdeParcelas; i++)
+            {
+                listBox1.Items.Add(String.Format("{0} - {1:C2} - {2}", i + 1, valorParcela, dataParcela.ToString("dd/MM/yyyy")));
+                dataParcela = gerarData(dataParcela, 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resetar();
+            // Valida se uma parcela foi selecionada
+            indexP = listBox1.SelectedIndex;
+            if (indexP == -1)
+            {
+                label2.Text = "Selecione uma parcela para pagar.";
+                return;
+            }
+            else if (indexP != 0)
+            {
+                label2.Text = "Pague a parcela anterior, antes de efetuar a compra dessa.";
+                return;
+            }
+
+            // Pega a data do dia do pagamento
+            DateTime dataPagamento = dateTimePicker2.Value.Date;
+            DateTime dataParcela = gerarData(dataCompra, qtdeParcelas - listBox1.Items.Count);
+
+            if (DateTime.Compare(dataParcela, dataPagamento) < 0)
+            {
+                double juros = valorParcela * 0.03;
+                label2.ForeColor = Color.Red;
+                label2.Text = String.Format("Parcela Atrasada!\nValor da parcela reajustado para {0:C2}\nDeseja Pagar?", valorParcela + juros);
+            }
+            else
+            {
+                label2.Text = String.Format("Parcela de {0:C2}\nDeseja Pagar?", valorParcela);
+            }
+            button3.Visible = true;
+            button4.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            valorTotal -= valorParcela;
+            label3.Text = String.Format("Total a Pagar: {0:C2}", valorTotal);
+            listBox1.Items.RemoveAt(indexP);
+            label2.Text = "Parcela Paga.";
+            resetar();
+            label2.ForeColor = Color.Green;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            resetar();
+            label2.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (valorTotal == 0)
+            {
+                label2.Text = "Nenhuma parcela para pagar.";
+                return;
+            }
+            valorTotal = 0;
+            label3.Text = String.Format("Total a Pagar: {0:C2}", valorTotal);
+            label2.Text = "Todas as parcelas pagas.";
+            listBox1.Items.Clear();
+            resetar();
+            label2.ForeColor = Color.Green;
+        }
+
+        private void resetar()
+        {
+            label1.Text = "";
+            label2.ForeColor = Color.Black;
+            button3.Visible = false;
+            button4.Visible = false;
+            indexP = 0;
+        }
+
+        private DateTime gerarData(DateTime data, int mes)
+        {
+            for (int i = 0; i < mes; i++)
+            {
+                data = data.AddMonths(1);
+                int semana = (int)data.DayOfWeek;
+                if (semana == 0)
+                {
+                    data = data.AddDays(1);
+                }
+                else if (semana == 6)
+                {
+                    data = data.AddDays(2);
+                }
+            }
+
+            return data;
+        }
+    }
+}
